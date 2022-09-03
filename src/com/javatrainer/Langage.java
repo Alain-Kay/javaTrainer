8103d76ee31fffbb3960f445e189cd6bd789a585
Alain-Kay <kazadikayumba@gmail.com>
2022-09-03 22:41:23 +0200

    notions des enumerations

--- src/com/javatrainer/Langage.java
+++ src/com/javatrainer/Langage.java
@@ -1,2 +1,24 @@
-package com.javatrainer;public enum Langage {
+package com.javatrainer;
+
+public enum Langage {
+    PHP ("langage PHP", "PHPSTORM"),
+    PYTHON ("langage PYTHON", "ANACONDA"),
+    JAVA ("langage JAVA", "INTELLIJI");
+
+   private String name = "";
+   private String editor = "";
+    Langage(String name, String editor){
+        this.name = name;
+        this.editor = editor;
+
+    }
+
+    public void getEditor() {
+        System.out.println("Editor : "+ editor);
+    }
+
+    @Override
+    public String toString() {
+        return name.toString();
+    }
 }
